bba76042e9db8a1ce24bcadf05a050e7ae417568
harshendravarma <47440162+harshendravarma@users.noreply.github.com>
2019-09-18 12:51:55 +0530

    added comments

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ package assignment6;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Exam[] exams = new Exam[3];
+		
 		for (Exam exam : exams) {
-			/// prints all three as null
-			System.out.println(exam);
+			System.out.println(exam); 	// prints all three as null as only references are created
 		}
+		
 		exams[0] = new Exam("quaterly");
 		exams[1] = new Exam("halfyearly");
 		exams[2] = new Exam("final");
